b8160899cf9224ca9dbee982ac918055bc13b2f7
zyoes <2843540362@qq.com>
2026-05-20 20:39:30 +0800

    初始化

---
+++ src/main/java/com/example/novamall/NovaMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.novamall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovaMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/novamall/NovaMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.novamall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
