49fc0573dae71d66b2a048ebdefd80b2ec5ad80c
蔡文哲 <CC_Amoy@outlook.com>
2026-05-29 23:31:31 +0800

    feat(user): 增加 SysUser 的实体类和其 Mapper 和 LoginRequest 、LoginResponse 和 LoginResponse 类用于接收和返回数据

---
+++ user/src/main/java/com/example/user/dao/request/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.example.user.dao.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    @NotBlank(message = "邮箱不能为空！")
+    @Email(message = "邮箱格式不正确")
+    private String email;
+
+    @NotBlank(message = "密码不能为空！")
+    private String password;
+
+    private String name;
+
+    private String mobile;
+}
---
+++ user/src/main/java/com/example/user/dao/response/LoginResponse.java
@@ -0,0 +1,18 @@
+package com.example.user.dao.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+    private String accessToken;
+
+    private String tokenType = "Bearer";
+
+    private Long expiresIn;
+
+    private String userId;
+
+    private String email;
+}
